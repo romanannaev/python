75730fc15435431ded5636bf56b7307d9d3db16b
Roman Annaev <romanannaev1992@gmail.com>
2019-07-21 18:33:34 +0300

    without bot_telegramm

--- myProject/main.py
+++ myProject/main.py
@@ -1,46 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import psycopg2
+import re
+
+
 
 
 def get_response(url):
+    """create object soup"""
+
     response = requests.get(url)
     return response.text
 
-def write_csv(current_kurs):
-    with open('D:/Python/myProject/select.by' , 'a') as f:
-        writer = csv.writer(f)
-
-        writer.writerow((current_kurs['usd_client'],
-                        current_kurs['usd_bank'],
-                        current_kurs['euro_client'],
-                        current_kurs['euro_bank'],
-                        current_kurs['rub_client'],
-                        current_kurs['rub_bank']))
-
-
-
 
 def get_total_info(html):
+    """parsing site"""
+
     soup = BeautifulSoup(html, 'lxml')
-    kurs = soup.find('tbody').find('tr').find_all ('td')[2 : 8]
-    total_kurs = [i.__str__().replace('td', '').replace('>', '').replace('<', '').replace('/', '') for i in kurs]
-    
-    current_kurs = {'usd_client' : total_kurs[0] ,
-                    'usd_bank' : total_kurs[1],
-                    'euro_client' : total_kurs[2],
-                    'euro_bank' : total_kurs[3],
-                    'rub_client' : total_kurs[4],
-                    'rub_bank' : total_kurs[5]}
-   
-    write_csv(current_kurs)
+
+    regexp = r'\d{1}'  
+    kurs = soup.find('tbody').find('tr').find_all(text = re.compile(regexp))
+    #total_kurs = [i.__str__().replace('td', '').replace('>', '').replace('<', '').replace('/', '') for i in kurs]
+    return kurs
 
 
+def insert_table(info):
+    """insert total_kurs in table"""
+    con = psycopg2.connect(
+    database = 'postgres',
+    user = 'postgres',
+    password = '20021992',
+    host = '127.0.0.1',
+    port = '5432'
+    )
+    print ('database opened successufully')
+
+    cur = con.cursor()
+
+    cur.execute('insert into all_currency(usd_client, usd_bank, euro_client, euro_bank, rub_client, rub_bank) \
+    values(%s,%s,%s,%s,%s,%s)', info)
+
+    con.commit()
+    print('record inserted successfully')
+    con.close()
 
 
 def main():
     url = 'https://select.by/kurs/'
-    get_total_info(get_response(url))
+    total_gen = get_total_info(get_response(url))
+    print(total_gen)
+    insert_table(total_gen)
+    
 
 
 if __name__ == "__main__":
